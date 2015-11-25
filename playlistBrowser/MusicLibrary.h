fb823c4cd357906dd3eb1ddd1dbf08a6a159310f
randallkanna <randallkanna@gmail.com>
2015-11-24 21:08:38 -0800

    addition of music library files

---
+++ playlistBrowser/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  playlistBrowser
+//
+//  Created by Randall Kanna on 11/24/15.
+//  Copyright (c) 2015 Randall Kanna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong, nonatomic) NSArray *library;
+
+@end
---
+++ playlistBrowser/MusicLibrary.m
@@ -0,0 +1,22 @@
+//
+//  MusicLibrary.m
+//  playlistBrowser
+//
+//  Created by Randall Kanna on 11/24/15.
+//  Copyright (c) 2015 Randall Kanna. All rights reserved.
+//
+
+#import "MusicLibrary.h"
+
+@implementation MusicLibrary
+
+- (instancetype)init {
+    self = [super init];
+    
+    if (self) {
+       // set up the dictionary
+    }
+    return self;
+}
+
+@end
