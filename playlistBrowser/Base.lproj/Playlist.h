486626dfb6da7bd53f0ed5ed9f97a61519e53896
randallkanna <randallkanna@gmail.com>
2015-11-25 15:10:49 -0800

    addition of playlists

--- playlistBrowser/Base.lproj/Playlist.h
+++ playlistBrowser/Base.lproj/Playlist.h
@@ -7,9 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface Playlist : NSObject
 
-@property (strong, nonatomic)
+@property (strong, nonatomic) NSString *playlistTitle;
+@property (strong, nonatomic) NSString *playlistDescription;
+@property (strong, nonatomic) UIImage *playlistIcon;
+@property (strong, nonatomic) UIImage *playlistIconLarge;
+@property (strong, nonatomic) NSArray *playlistArtists;
+@property (strong, nonatomic) UIColor *backgroundColor;
 
 @end
