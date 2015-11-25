3ba9555e89bae0076968593cad4f93efc375c010
randallkanna <randallkanna@gmail.com>
2015-11-25 15:04:52 -0800

    addition of music library

---
+++ playlistBrowser/Base.lproj/Playlist.h
@@ -0,0 +1,15 @@
+//
+//  Playlist.h
+//  playlistBrowser
+//
+//  Created by Randall Kanna on 11/25/15.
+//  Copyright (c) 2015 Randall Kanna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Playlist : NSObject
+
+@property (strong, nonatomic)
+
+@end
---
+++ playlistBrowser/Base.lproj/Playlist.m
@@ -0,0 +1,13 @@
+//
+//  Playlist.m
+//  playlistBrowser
+//
+//  Created by Randall Kanna on 11/25/15.
+//  Copyright (c) 2015 Randall Kanna. All rights reserved.
+//
+
+#import "Playlist.h"
+
+@implementation Playlist
+
+@end
