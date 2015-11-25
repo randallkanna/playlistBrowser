75fc7637f9fbee1ab53ca16c5339954e8f07ee8d
randallkanna <randallkanna@gmail.com>
2015-11-24 16:55:41 -0800

    segue

--- playlistBrowser/PlaylistDetailViewController.h
+++ playlistBrowser/PlaylistDetailViewController.h
@@ -10,4 +10,6 @@
 
 @interface PlaylistDetailViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *buttonPressLabel;
+
 @end
--- playlistBrowser/PlaylistMasterViewController.m
+++ playlistBrowser/PlaylistMasterViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "PlaylistMasterViewController.h"
+#import "PlaylistDetailViewController.h"
 
 @interface PlaylistMasterViewController ()
 
@@ -26,7 +27,7 @@
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqual:@"showPlaylistDetail"]) {
-        // do something
+        PlaylistDetailViewController *playlistDetailController = (PlaylistDetailViewController *)segue.destinationViewController;
     }
 }
 
